01701e7405d52550b47cc1c97db70d1309d91ddc
cgonzport <38139317+cgonzport@users.noreply.github.com>
2019-05-17 21:30:20 +0200

    Nuevo Formulario para  control de Mensajes

---
+++ WFChamilo6/Frms/frmMensajes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFChamilo6.Frms
+{
+    public partial class frmMensajes : Form
+    {
+        public frmMensajes()
+        {
+            InitializeComponent();
+        }
+    }
+}
